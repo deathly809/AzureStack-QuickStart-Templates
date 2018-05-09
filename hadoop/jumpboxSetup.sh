7c20d949537cc6f4ccc0dcfafb4fa7d2df063a7c
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-05-08 17:46:58 -0700

    adsf

--- hadoop/jumpboxSetup.sh
+++ hadoop/jumpboxSetup.sh
@@ -52,8 +52,8 @@ NODES=("${CLUSTER_NAME}NameNode" "${CLUSTER_NAME}ResourceManager" "${CLUSTER_NAM
 # Add workers
 for i in `seq 1 $NUMBER_NODES)`
 do
-	worker="${CLUSTER_NAME}Worker$i"
-	NODES[$((i + 4))]=$worker
+    worker="${CLUSTER_NAME}Worker$i"
+    NODES[$((i + 4))]=$worker
 done
 
 
@@ -63,12 +63,12 @@ done
 #	cluster
 #
 preinstall () {
-	# Install avahi-daemon and Java Runtime Environment
-	sudo apt-get update
-	sudo apt-get install --yes default-jre sshpass
+    # Install avahi-daemon and Java Runtime Environment
+    sudo apt-get update
+    sudo apt-get install --yes default-jre sshpass
 
-	# Setup JAVA
-	echo -e "JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')" >> /etc/profile
+    # Setup JAVA
+    echo -e "JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')" >> /etc/profile
 }
 
 
@@ -77,14 +77,15 @@ preinstall () {
 # 	Copy public keys from all nodes to all other nodes.
 #
 copy_users () {
-    for FROM in ${NODES[@]} do
-        for TO in ${NODES[@]} do
-            for U in ${USERS[@]} do
+    for FROM in ${NODES[@]}; do
+        for TO in ${NODES[@]}; do
+            for U in ${USERS[@]}; do
+
                 # Copy public key here
                 sshpass -p $PASSWORD scp $FROM:/home/$U/.ssh/id_rsa.pub .
 
                 # Add to remove authorized_keys
-                cat id_rsa.pub >> sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $TO 'cat >> /home/$U/.ssh/authorized_keys'
+                cat id_rsa.pub | sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $TO 'cat >> /home/$U/.ssh/authorized_keys'
 
                 # Remove public key
                 rm -f id_rsa.pub
@@ -99,7 +100,7 @@ copy_users () {
 #   cause Hadoop to start on each node.
 #
 restart_nodes () {
-    for N in ${NODES[@]} do
+    for N in ${NODES[@]}; do
         sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $TO 'echo $PASSWORD | sudo -S restart'
     done
 }
