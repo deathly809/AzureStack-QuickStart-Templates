6e660f7dd9cf867929b7ed6996e45d70131f2d23
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-06-27 15:25:19 -0700

    Full path

--- terasort/scripts/terasort.sh
+++ terasort/scripts/terasort.sh
@@ -21,13 +21,13 @@ SORTED=/home/hadoop/sorted
 VALIDATED=/home/hadoop/validated
 
 # Teragen
-yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS teragen 10000000000 $UNSORTED > teragen.stdout 2> teragen.stderr
+/usr/local/hadoop/bin/yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS teragen 10000000000 $UNSORTED > teragen.stdout 2> teragen.stderr
 
 # Terasort
-yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS terasort $UNSORTED $SORTED > terasort.stdout 2> terasort.stderr
+/usr/local/hadoop/bin/yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS terasort $UNSORTED $SORTED > terasort.stdout 2> terasort.stderr
 
 # Teravalidate
-yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS teravalidate $SORTED $VALIDATED > teravalidate.stdout 2> teravalidate.stderr
+/usr/local/hadoop/bin/yarn jar $EXAMPLES -Dmapreduce.job.maps=MAPPERS -Dmapreduce.job.reduces=REDUCERS teravalidate $SORTED $VALIDATED > teravalidate.stdout 2> teravalidate.stderr
 
 # Remove all at jobs in the queue
 for i in `atq | awk '{print $1}'`;do atrm $i;done
